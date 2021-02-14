1fca53d79790726a902519fc3b52af87fcde2e51
lililiwuming <lilili.wu.ming@outlook.com>
2021-02-14 20:48:04 +0800

    remove storefile

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM alpine:latest
 
 ADD configure.sh /configure.sh
-
-RUN apk add --no-cache ca-certificates caddy wget unzip && \
+mkdir -p /xray
+RUN apk add --no-cache ca-certificates caddy curl unzip && \
 chmod +x /configure.sh && \
 rm -rf /var/cache/apk/*
 
--- configure.sh
+++ configure.sh
@@ -1,29 +1,24 @@
 #!/bin/sh
 
 # Download and install Xray
-wget https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip -O /xray.zip
-unzip /xray.zip -d /
-install -m 755 /xray /
+mkdir /tmp/xray
+curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+unzip /tmp/xray/xray.zip -d /tmp/xray
+install -m 755 /tmp/xray/xray /xray
+install -m 755 /tmp/xray/ge* /xray
 
 # Remove temporary directory
-rm -rf /xray.zip
+rm -rf /tmp/xray
 
 # configs
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
-wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
-wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
-wget $CONFIGXRAY -O/xray.json
+curl -L -H "Cache-Control: no-cache"  $CADDYIndexPage -o /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
+curl -L -H "Cache-Control: no-cache"  $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
+curl -L -H "Cache-Control: no-cache"  $CONFIGXRAY -o /xray/xray.json
 
-# storefiles
-mkdir -p /usr/share/caddy/$AUUID && wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles
-wget -P /usr/share/caddy/$AUUID -i /usr/share/caddy/$AUUID/StoreFiles
-
-for file in $(ls /usr/share/caddy/$AUUID); do
-    [[ "$file" != "StoreFiles" ]] && echo \<a href=\""$file"\" download\>$file\<\/a\>\<br\> >>/usr/share/caddy/$AUUID/ClickToDownloadStoreFiles.html
-done
 
 # start
 
-/xray -config /xray.json &
+/xray/xray -config /xray/xray.json &
 
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
