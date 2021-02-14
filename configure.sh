cfa0d34489664b1037c4391d724d7572253b7b50
lililiwuming <lilili.wu.ming@outlook.com>
2021-02-14 20:25:30 +0800

    update

--- configure.sh
+++ configure.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
 # Download and install Xray
-wget -qO- https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip 
-unzip /Xray-linux-64.zip  -d /
+wget https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip -O /xray.zip
+unzip /xray.zip -d /
+install -m 755 /xray /
 
 # Remove temporary directory
-rm -rf /Xray-linux-64.zip
+rm -rf /xray.zip
 
 # configs
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
-wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/xray.json
+wget $CONFIGXRAY -O/xray.json
 
 # storefiles
 mkdir -p /usr/share/caddy/$AUUID && wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles
