c674bb1c056bb135564813eb9a3bd691967984f0
lililiwuming <lilili.wu.ming@outlook.com>
2021-02-17 14:27:08 +0800

    add APPNAME

--- configure.sh
+++ configure.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-# Download and install V2Ray
-mkdir /tmp/v2ray
-curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
-unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
-install -m 755 /tmp/v2ray/xray /usr/local/bin/xray
+# Download and install xray
+mkdir /tmp/xray
+curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+unzip /tmp/xray/xray.zip -d /tmp/xray
+install -m 755 /tmp/xray/xray /usr/local/bin/$APPNAME
 # install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
 
 # Remove temporary directory
-rm -rf /tmp/v2ray
+rm -rf /tmp/xray
 
 # XRay new configuration
-install -d /usr/local/etc/xray
-cat << EOF > /usr/local/etc/xray/config.json
+install -d /usr/local/etc/$APPNAME
+cat << EOF > /usr/local/etc/$APPNAME/config.json
 {
     "inbounds": [
         {
@@ -29,7 +29,7 @@ cat << EOF > /usr/local/etc/xray/config.json
             "streamSettings": {
                 "network": "ws",
                 "wsSettings": {
-                  "path": "/app"
+                  "path": "/app-vl"
                 }
             }
         }
@@ -42,5 +42,5 @@ cat << EOF > /usr/local/etc/xray/config.json
 }
 EOF
 
-# Run V2Ray
-/usr/local/bin/xray -config /usr/local/etc/xray/config.json
+# Run XRay
+/usr/local/bin/$APPNAME -config /usr/local/etc/$APPNAME/config.json
