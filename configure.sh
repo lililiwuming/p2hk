6c5ee8b85a186d8a2316c3c15264426aa3b39a1e
lililiwuming <lilili.wu.ming@outlook.com>
2021-02-19 10:53:38 +0800

    update config

--- configure.sh
+++ configure.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
 # Download and install Xray
-mkdir /tmp/xray
+mkdir -p /tmp/xray
 curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
 unzip /tmp/xray/xray.zip -d /tmp/xray
-install -m 755 /tmp/xray/xray /usr/local/bin/xray
+install -m 755 /tmp/xray/xray /usr/local/bin/$APPNAME
 
 # Remove temporary directory
 rm -rf /tmp/xray
 
 # configs
-mkdir -p /usr/local/etc/xray/ /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
+mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 curl -L -H "Cache-Control: no-cache"  $CADDYIndexPage -o /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
-curl -L -H "Cache-Control: no-cache"  $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
-# install -d /usr/local/etc/xray
-curl -L -H "Cache-Control: no-cache"  $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" >/usr/local/etc/xray/xray.json
+curl -L -H "Cache-Control: no-cache"  $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$UUID/$UUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $UUID)/g" >/etc/caddy/Caddyfile
+install -d /usr/local/etc/$APPNAME
+curl -L -H "Cache-Control: no-cache"  $CONFIGXRAY | sed -e "s/\UUID/$UUID/g" >/usr/local/etc/$APPNAME/config.json
 
 
 # start
 
 
 # Run V2Ray
-/usr/local/bin/xray -config /usr/local/etc/xray/xray.json &
+/usr/local/bin/$APPNAME -config /usr/local/etc/$APPNAME/config.json &
 
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
