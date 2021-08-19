b603e3d7a7f8482c17c941e1e5029d6aa5d588fd
lililiwuming <62045515+lililiwuming@users.noreply.github.com>
2021-08-20 07:14:39 +0800

    up v2 vmess

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Global variables
-DIR_CONFIG="/etc"
+DIR_CONFIG="/etc/{APPNAME}"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
 
@@ -35,12 +35,12 @@ curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.co
 busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
 
 # Convert to protobuf format configuration
-mkdir -p ${DIR_CONFIG}/{APPNAME}
-${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/{APPNAME}/config.pb
+mkdir -p ${DIR_CONFIG}
+${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
 
 # Install V2Ray
-install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
+install -m 755 ${DIR_TMP}/{APPNAME} ${DIR_RUNTIME}
 rm -rf ${DIR_TMP}
 
 # Run V2Ray
-${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/{APPNAME}/config.pb
+${DIR_RUNTIME}/{APPNAME} -config=${DIR_CONFIG}/config.pb
